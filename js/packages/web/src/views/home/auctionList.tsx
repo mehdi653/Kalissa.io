11ce2a2582d2a39a18fb7ea3cb687786159aadb9
mehdi653 <acer653@me.com>
2021-10-24 03:45:10 +0200

    Bug Fixed21

--- js/packages/web/src/views/home/auctionList.tsx
+++ js/packages/web/src/views/home/auctionList.tsx
@@ -147,13 +147,6 @@ export const AuctionListView = () => {
         useBannerBg={true}
       />
       <Layout>
-       <Col style={{ width: '100%', marginTop: 32 }}>
-          <Row>
-            <Col xs>First, but unordered</Col>
-            <Col xs={{ order: 12 }}>Second, but last</Col>
-            <Col xs={{ order: 1 }}>Third, but second</Col>
-          </Row>
-        </Col>
         <Content style={{ display: 'flex', flexWrap: 'wrap' }}>
           <Col style={{ width: '100%', marginTop: 32 }}>
             <Row>
