6f5262cc8c34bca846ae23355f4e8f212a04c5ae
mehdi653 <acer653@me.com>
2021-10-26 01:47:59 +0200

    23

--- js/packages/web/src/views/home/auctionList.tsx
+++ js/packages/web/src/views/home/auctionList.tsx
@@ -152,7 +152,13 @@ export const AuctionListView = () => {
       />
       <h1>Welcome to the first collector's item brand supported by the NFT</h1>
       <p className="textarea">The kalissa digital and physical collectibles are available in limited editions. They can be bought, sold and exchanged. </p>
-      
+      <Banner
+        src={''}
+        headingText={''}
+        subHeadingText={''}
+        actionComponent={<HowToBuyModal buttonClassName="secondary-btn" />}
+        useBannerBg={false}
+      />
       
     
       
